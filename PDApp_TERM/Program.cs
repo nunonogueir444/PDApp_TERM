df31c8f759e63b0bb6c13233124f08494a598e0a
nunonogueir444 <nunonogueir444@gmail.com>
2025-02-15 12:19:41 +0000

    update

--- PDApp_TERM/Program.cs
+++ PDApp_TERM/Program.cs
@@ -10,6 +10,25 @@ namespace PDApp_TERM
     {
         static void Main(string[] args)
         {
+            //#########################################################################################
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Please provide two numeric arguments.");
+                return;
+            }
+
+            if (int.TryParse(args[0], out int num1) && int.TryParse(args[1], out int num2))
+            {
+                int sum = num1 + num2;
+                Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
+            }
+            else
+            {
+                Console.WriteLine("Both arguments must be valid integers.");
+            }
+
+            //#########################################################################################
         }
     }
 }
