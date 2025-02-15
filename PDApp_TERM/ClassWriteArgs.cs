b35e94756c929c53b9003761e3621eb43ae6f3a0
nunonogueir444 <nunonogueir444@gmail.com>
2025-02-15 23:35:07 +0000

    update

---
+++ PDApp_TERM/ClassWriteArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDApp_TERM
+{
+    class ClassWriteArgs
+    {
+        public static void WriteArgs(string[] args)
+        {
+
+        //#########################################################################################
+
+            if (args.Length < 4)
+            {
+                Console.WriteLine("Args missing!");
+                return;
+            }
+
+            Console.WriteLine("Serial:" + args[0]
+                              + "   Model:" + args[1]
+                              + "   Matics:" + args[2]
+                              + "   Bat:" + args[3]);
+        
+
+        //#########################################################################################
+
+        }
+    }
+}
--- PDApp_TERM/Program.cs
+++ PDApp_TERM/Program.cs
@@ -1,39 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PDApp_TERM
+﻿namespace PDApp_TERM
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //#########################################################################################
 
-            if (args.Length < 4)
-            {
-                Console.WriteLine("Please provide all arguments.");
-                return;
-            }
+        //#########################################################################################
 
-            /*if (int.TryParse(args[0], out int num1) && int.TryParse(args[1], out int num2))
-            {
-                int sum = num1 + num2;
-                Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
-            }
-            else
-            {
-                Console.WriteLine("Both arguments must be valid integers.");
-            }*/
+            ClassWriteArgs writer = new();
+            ClassWriteArgs.WriteArgs(args);
 
-            Console.WriteLine("Arguments provided: " + "Serial:" + args[0] 
-                                                     + "   Model:" + args[1] 
-                                                     + "   Matics:" + args[2] 
-                                                     + "   Bat:" + args[3]);
+        //#########################################################################################
 
-            //#########################################################################################
         }
     }
 }
