6b9373b692123528570fef67cdb0e17b4e4b2b12
nunonogueir444 <nunonogueir444@gmail.com>
2025-02-15 15:36:16 +0000

    update

--- PDApp_TERM/Program.cs
+++ PDApp_TERM/Program.cs
@@ -12,13 +12,13 @@ namespace PDApp_TERM
         {
             //#########################################################################################
 
-            if (args.Length < 2)
+            if (args.Length < 4)
             {
-                Console.WriteLine("Please provide two numeric arguments.");
+                Console.WriteLine("Please provide all arguments.");
                 return;
             }
 
-            if (int.TryParse(args[0], out int num1) && int.TryParse(args[1], out int num2))
+            /*if (int.TryParse(args[0], out int num1) && int.TryParse(args[1], out int num2))
             {
                 int sum = num1 + num2;
                 Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
@@ -26,7 +26,12 @@ namespace PDApp_TERM
             else
             {
                 Console.WriteLine("Both arguments must be valid integers.");
-            }
+            }*/
+
+            Console.WriteLine("Arguments provided: " + "Serial:" + args[0] 
+                                                     + "   Model:" + args[1] 
+                                                     + "   Matics:" + args[2] 
+                                                     + "   Bat:" + args[3]);
 
             //#########################################################################################
         }
